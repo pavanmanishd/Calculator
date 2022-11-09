de7ceb2ed93a1f0b8b50f1104263af9911c58e9a
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-11-09 20:29:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var input = document.getElementById("input");
+var number = document.getElementsByClassName("num");
+var op = document.getElementsByClassName("operators");
+var result = document.getElementById("result");
+var clear = document.getElementById("clear");
+var resultDisplayed = false;
+
+
