ba64be773439e564c302f7e85c0e36ddf29675f2
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-11-11 19:14:47 +0530

    adding Functionality

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var input = document.getElementById("input");
-var number = document.getElementsByClassName("num");
-var op = document.getElementsByClassName("operators");
-var result = document.getElementById("result");
-var clear = document.getElementById("clear");
-var resultDisplayed = false;
-
-
+var a = 1;
+document.addEventListener("keypress", function (event) {
+    if (a === 1) {
+        if (event.key === 'Enter') {
+            a = 1;
+            Calculator(document.querySelector("input").value);
+        }
+    }
+    else if (a === 0) {
+        document.querySelector("input").value = ' ';
+        a = 1
+    }
+    if(document.activeElement.tagName !== 'INPUT'){
+        document.querySelector("input").focus();   
+    }
+})
+function Calculator(expression) {
+    var x = eval(expression);
+    document.querySelector("input").value = x;
+    a = 0
+}
